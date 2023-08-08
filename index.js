80afb7148f71fd445543399cb2d33ac7833bb7c8
Max I <clickkava@gmail.com>
2023-08-08 18:26:39 +0300

    Add an array and function to add books to

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
+const shelf = document.querySelector("main");
+
+let myLibrary = [];
+
 function Book(title, author, pages, read) {
     this.title = title
     this.author = author
     this.pages = pages
     this.read = read
-    this.info = function() {
-        return `${title} ${author} ${pages} ${read}`
-    }
-}
-  
-const theHobbit = new Book('The Hobbit', 'by J.R.R. Tolkien', '295 pages', 'not read yet');
+};
 
-console.log(theHobbit.info());
+function addBookToLibrary(input) {
+    const theHobbit = new Book('The Hobbit', 'by J.R.R. Tolkien', '295 pages', 'not read yet');
+    myLibrary.push(theHobbit);
+};
+
+function updateShelf() {
+    const bookElement = document.createElement('div');
+    shelf.appendChild(bookElement).classList.add("bookElement");
+};
+
+addBookToLibrary();
+console.log(myLibrary);
