ff7c2dc402a0f9db7f9d2fef589eb441390ea84b
Max I <clickkava@gmail.com>
2023-08-09 10:11:49 +0300

    Add a form to add books, receive and update data
    Take the user’s input and store the new book objects into an array.
    Loop through the array and display each book on the page with the
    appropriate classes.

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 const shelf = document.querySelector("main");
+const addBookForm = document.getElementById("addBookForm");
+const titleInput = document.getElementById("title");
+const authorInput = document.getElementById("author");
+const numOfPagesinput = document.getElementById("pages");
+const wantToReadInput = document.getElementById("want");
+const currentlyReadingInput = document.getElementById("reading");
+const readInput = document.getElementById("read");
 
 let myLibrary = [];
 
@@ -9,30 +16,62 @@ function Book(title, author, pages, read) {
     this.read = read
 };
 
-function addBookToLibrary(input) {
+function addBookToLibrary(title, author, pages, read) {
     const theHobbit = new Book('The Hobbit', 'by J.R.R. Tolkien', '295 pages', 'not read yet');
-    myLibrary.push(theHobbit);
+    let addNewBook = new Book(title, author, pages, read);
+    myLibrary.push(addNewBook);
+    updateShelf();
 };
 
 function updateShelf() {
+    const currentShelf = document.querySelectorAll(".book-element");
+    currentShelf.forEach(book => {
+        book.remove();
+    });
+
     myLibrary.forEach(book => {
         const bookElement = document.createElement("div");
         shelf.appendChild(bookElement).classList.add("book-element");
+        console.log(book);
 
         bookElement.appendChild(document.createElement("p")).classList.add("book-title");
-        document.querySelector(".book-title").innerText = `${book.title}`;
+        const title = document.querySelectorAll(".book-title");
+        title[title.length - 1].innerText = `${book.title}`
 
         bookElement.appendChild(document.createElement("p")).classList.add("book-author");
-        document.querySelector(".book-author").innerText = `${book.author}`;
+        const author = document.querySelectorAll(".book-author");
+        author[author.length - 1].innerText = `${book.author}`;
 
         bookElement.appendChild(document.createElement("p")).classList.add("book-pages");
-        document.querySelector(".book-pages").innerText = `${book.pages}`;
+        const pages = document.querySelectorAll(".book-pages");
+        pages[pages.length - 1].innerText = `${book.pages}`;
 
         bookElement.appendChild(document.createElement("p")).classList.add("book-read");
-        document.querySelector(".book-read").innerText = `${book.read}`;
+        const read = document.querySelectorAll(".book-read");
+        read[read.length - 1].innerText = `${book.read}`;
+
+
+        // bookElement.appendChild(document.createElement("p")).innerText = `${book.title}`;
+
+        // bookElement.appendChild(document.createElement("p")).innerText = `${book.author}`;
+
+        // bookElement.appendChild(document.createElement("p")).innerText = `${book.pages}`;
+
+        // bookElement.appendChild(document.createElement("p")).innerText = `${book.read}`;
     });
 };
 
-addBookToLibrary();
-updateShelf();
+addBookForm.addEventListener("submit", function(e) {
+    e.preventDefault();
+
+    let chosenReadState = "";
+
+    wantToReadInput.checked ? chosenReadState = "Want to read" : null;
+    currentlyReadingInput.checked ? chosenReadState = "Currently reading" : null;
+    readInput.checked ? chosenReadState = "Read" : null;
+
+    addBookToLibrary(titleInput.value, authorInput.value, numOfPagesinput.value, chosenReadState);
+});
+
+
 console.log(myLibrary);
