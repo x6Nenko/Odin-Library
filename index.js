523cf6afd08f438908772821fe98ab91ec887831
Max I <clickkava@gmail.com>
2023-08-09 19:39:49 +0300

    Add a btn for each book to change its read status

--- index.js
+++ index.js
@@ -64,19 +64,38 @@ function updateShelf() {
         bookElement.appendChild(document.createElement("button")).classList.add("deleteBtn");
         const deleteBtn = document.querySelectorAll(".deleteBtn");
         deleteBtn[deleteBtn.length - 1].innerText = `Delete book`;
+
+        bookElement.appendChild(document.createElement("div")).classList.add("input-container");
+        const inputContainer = document.querySelectorAll(".input-container");
+        inputContainer[inputContainer.length - 1].appendChild(document.createElement("label")).htmlFor = `switchState${index}`;
+        inputContainer[inputContainer.length - 1].appendChild(document.createElement("select")).id = `switchState${index}`;
+        const latestSelect = document.querySelectorAll("select");
+        const selectOption1 = document.createElement("option");
+        selectOption1.value = "Want to read";
+        selectOption1.text = "Want to read";
+        selectOption1.value === myLibrary[index].read ? selectOption1.setAttribute("selected" , "selected") : null;
+        const selectOption2 = document.createElement("option");
+        selectOption2.value = "Currently reading";
+        selectOption2.text = "Currently reading";
+        selectOption2.value === myLibrary[index].read ? selectOption2.setAttribute("selected" , "selected") : null;
+        const selectOption3 = document.createElement("option");
+        selectOption3.value = "Read";
+        selectOption3.text = "Read";
+        selectOption3.value === myLibrary[index].read ? selectOption3.setAttribute("selected" , "selected") : null;
+        latestSelect[latestSelect.length -1].appendChild(selectOption1);
+        latestSelect[latestSelect.length -1].appendChild(selectOption2);
+        latestSelect[latestSelect.length -1].appendChild(selectOption3);
+        latestSelect[latestSelect.length -1].addEventListener("click", function(e) {
+            console.log(myLibrary[index]);
+            myLibrary[index].read = e.target.value;
+            console.log(myLibrary[index]);
+        });
     });
 };
 
 addBookForm.addEventListener("submit", function(e) {
     e.preventDefault();
-
     let chosenReadState = "";
-
-    wantToReadInput.checked ? chosenReadState = "Want to read" : null;
-    currentlyReadingInput.checked ? chosenReadState = "Currently reading" : null;
-    readInput.checked ? chosenReadState = "Read" : null;
-
-    console.log(stateSelectInput.value);
     
     addBookToLibrary(coverInput.value, titleInput.value, authorInput.value, numOfPagesinput.value, stateSelectInput.value);
 });
